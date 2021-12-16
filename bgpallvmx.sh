aca529c12b62fe1b16313b9185d0e3ca47a6f271
delphae <45516472+Delphae@users.noreply.github.com>
2021-12-16 21:52:35 +0100

    Add files via upload

---
+++ bgpallvmx.sh
@@ -0,0 +1,14 @@
+resourcegroup="resource-group-01"
+peer1="peer-vmx1"
+peer2="peer-vmx2-spoke"
+routeserver="sdwan-routeserver"
+
+echo "peer:" $peer1
+az network routeserver peering list-learned-routes \
+-n $peer1 --routeserver $routeserver -g resource-group-01 \
+--query 'RouteServiceRole_IN_0' -o table
+
+echo "peer:" $peer2
+az network routeserver peering list-learned-routes \
+-n $peer2 --routeserver $routeserver -g resource-group-01 \
+--query 'RouteServiceRole_IN_0' -o table
